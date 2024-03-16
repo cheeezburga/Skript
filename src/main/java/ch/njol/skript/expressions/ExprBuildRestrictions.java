831aee8a79923e3104daa6f7996388467841fcfa
cheeezburga <47320303+cheeezburga@users.noreply.github.com>
2024-03-16 16:22:45 +1000

    Renamed and merged destroyable/placeable keys expressions into ExprBuildRestrictions. - Has two patterns instead of one with a parse tag. - Unsure if this would be the best way to implement this.

--- src/main/java/ch/njol/skript/expressions/ExprBuildRestrictions.java
+++ src/main/java/ch/njol/skript/expressions/ExprBuildRestrictions.java
@@ -7,6 +7,7 @@ import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.skript.doc.*;
 import ch.njol.skript.expressions.base.PropertyExpression;
 import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
 import ch.njol.util.coll.CollectionUtils;
@@ -19,38 +20,43 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
 
-@Name("Placeable Keys Of Item")
-@Description("Get or modify the placeable keys of an item.")
-@Examples({"add dirt to place items of player's tool",
-	"add stone and diamond ore to place on blocks of {_item}",
-	"clear placeable items of {_item}",
-	"remove sand from place keys of {_item}"})
+@Name("Build Restrictions Of Item")
+@Description("Get or modify the build restrictions of an item.")
+@Examples({"add dirt to destroyable keys of player's tool",
+	"add stone and diamond ore to destroy blocks of {_item}",
+	"clear destroyable items of {_item}",
+	"remove sand from destroy keys of {_item}"})
 @Since("INSERT VERSION")
 @RequiredPlugins("Paper")
-public class ExprPlaceableKeys extends PropertyExpression<ItemType, ItemType> {
+public class ExprBuildRestrictions extends PropertyExpression<ItemType, ItemType> {
 
 	static {
-		if (Skript.methodExists(ItemMeta.class, "getPlaceableKeys")) {
-			register(ExprPlaceableKeys.class, ItemType.class, "place[able| on] (keys|items|blocks)", "itemtypes");
+		if (Skript.methodExists(ItemMeta.class, "getBuildRestrictions")) {
+			Skript.registerExpression(ExprBuildRestrictions.class, ItemType.class, ExpressionType.PROPERTY,
+				"[the] destroy[able] (key|block|item)[s] (of|on) %itemtypes%",
+				"[the] place[able| on] (key|block|item)[s] (of|on) %itemtypes%");
 		}
 	}
+
+	private static final int DESTROYABLE = 0;
+	private int pattern;
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
 		setExpr((Expression<ItemType>) exprs[0]);
+		this.pattern = matchedPattern;
 		return true;
 	}
 
-	@SuppressWarnings("")
 	@Override
 	protected ItemType @NotNull [] get(Event event, ItemType [] source) {
 		Set<ItemType> onItem = new HashSet<>();
 		for (ItemType item : source) {
 			if (item.getRandom().hasItemMeta()) {
 				ItemMeta meta = item.getItemMeta();
-				if (meta.hasPlaceableKeys()) {
-					for (Namespaced key : meta.getPlaceableKeys()) {
+				if (pattern == DESTROYABLE ? meta.hasDestroyableKeys() : meta.hasPlaceableKeys()) {
+					for (Namespaced key : pattern == DESTROYABLE ? meta.getDestroyableKeys() : meta.getPlaceableKeys()) {
 						Material material = BukkitUnsafe.getMaterialFromMinecraftId(key.toString());
 						if (material != null) {
 							onItem.add(new ItemType(material));
@@ -82,17 +88,19 @@ public class ExprPlaceableKeys extends PropertyExpression<ItemType, ItemType> {
 			for (ItemType item : source) {
 				ItemMeta meta = item.getItemMeta();
 				Collection<Namespaced> empty = new ArrayList<>();
-				meta.setPlaceableKeys(empty);
-				item.setItemMeta(meta);
+                if (pattern == DESTROYABLE) { meta.setDestroyableKeys(empty); } else { meta.setPlaceableKeys(empty); }
+                item.setItemMeta(meta);
 			}
 		} else if (mode == ChangeMode.SET || mode == ChangeMode.ADD || mode == ChangeMode.REMOVE) {
 
+			// only do this if the mode requires it
 			Set<Namespaced> keys = new HashSet<>();
 
+			// get a list of all the provided itemtypes' namespaced keys
 			if (delta != null) {
 				for (Object o : delta) {
 					if (o instanceof ItemType item) {
-						keys.add(item.getMaterial().getKey());
+						keys.add(item.getRandom().getType().getKey());
 					}
 				}
 			}
@@ -100,21 +108,21 @@ public class ExprPlaceableKeys extends PropertyExpression<ItemType, ItemType> {
 			if (mode == ChangeMode.SET) {
 				for (ItemType item : source) {
 					ItemMeta meta = item.getItemMeta();
-					meta.setPlaceableKeys(keys);
-					item.setItemMeta(meta);
+                    if (pattern == DESTROYABLE) { meta.setDestroyableKeys(keys); } else { meta.setPlaceableKeys(keys); }
+                    item.setItemMeta(meta);
 				}
 			} else if (mode == ChangeMode.ADD || mode == ChangeMode.REMOVE) {
 				for (ItemType item : source) {
 					ItemMeta meta = item.getItemMeta();
-					if (meta.hasPlaceableKeys()) {
-						Set<Namespaced> alreadyOn = meta.getPlaceableKeys();
+					if (pattern == DESTROYABLE ? meta.hasDestroyableKeys() : meta.hasPlaceableKeys()) {
+						Set<Namespaced> alreadyOn = pattern == DESTROYABLE ? meta.getDestroyableKeys() : meta.getPlaceableKeys();
 						if (mode == ChangeMode.ADD) {
 							alreadyOn.addAll(keys);
 						} else if (mode == ChangeMode.REMOVE) {
 							alreadyOn.removeAll(keys);
 						}
-						meta.setPlaceableKeys(alreadyOn);
-					}
+                        if (pattern == DESTROYABLE) { meta.setDestroyableKeys(alreadyOn); } else { meta.setPlaceableKeys(alreadyOn); }
+                    }
 
 					item.setItemMeta(meta);
 				}
@@ -134,7 +142,7 @@ public class ExprPlaceableKeys extends PropertyExpression<ItemType, ItemType> {
 
 	@Override
 	public @NotNull String toString(@Nullable Event e, boolean b) {
-		return "placeable keys of " + getExpr().toString(e, b);
+		return "build restrictions of " + getExpr().toString(e, b);
 	}
 
 }
