5c36cc42e1d714e1e5dae5ddcaad081834da5d14
cheeezburga <47320303+cheeezburga@users.noreply.github.com>
2024-03-23 01:01:54 +1000

    Changes and fixes to the condition - Removed star import - Changed patterns to be better and more readable (cheers Sovde) - Some formatting changes - Changed field names to better reflect what they are - Swapped order of stuff in check() - Changed logic in check to return false if the existing keys don't contain any of the keysToCheck, instead of returning true if they did - toString() method should now handle different cases better

--- src/main/java/ch/njol/skript/conditions/CondHasBuildRestrictions.java
+++ src/main/java/ch/njol/skript/conditions/CondHasBuildRestrictions.java
@@ -2,10 +2,14 @@ package ch.njol.skript.conditions;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.aliases.ItemType;
-import ch.njol.skript.doc.*;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.RequiredPlugins;
+import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Condition;
 import ch.njol.skript.lang.Expression;
-import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
 import com.destroystokyo.paper.Namespaced;
 import org.bukkit.event.Event;
@@ -25,29 +29,29 @@ import java.util.Set;
 @Since("INSERT VERSION")
 @RequiredPlugins("Paper")
 public class CondHasBuildRestrictions extends Condition {
+
 	static {
 		if (Skript.methodExists(ItemMeta.class, "hasDestroyableKeys")) {
 			Skript.registerCondition(CondHasBuildRestrictions.class,
 				"%itemtypes% (can|is able to) (break|destroy|mine|place:be placed on) %itemtypes%",
 				"%itemtypes% (can't|can[ ]not|is unable to) (break|destroy|mine|place:be placed on) %itemtypes%",
-				"%itemtypes% (has|have) [a|any] (destroy[able]|place:place[able| on]) (key|item|block)[s]",
-				"%itemtypes% (doesn't|does not|do not|don't) have [a|any] (destroy[able]|place:place[able| on]) (key|item|block)[s]",
-				"");
+				"%itemtypes% (has|have) [a|any] (break|place:build) restrictions",
+				"%itemtypes% (doesn't|does not|do not|don't) have [a|any] (break|place:build) restrictions");
 		}
 	}
 
 	@SuppressWarnings("NotNullFieldNotInitialized")
-	private Expression<ItemType> itemTypes;
+	private Expression<ItemType> items;
 	@SuppressWarnings("NotNullFieldNotInitialized")
-	private Expression<ItemType> keys;
+	private Expression<ItemType> keysToCheck;
 	private boolean place;
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-		this.itemTypes = (Expression<ItemType>) exprs[0];
+		this.items = (Expression<ItemType>) exprs[0];
 		if (matchedPattern == 0 || matchedPattern == 1)
-			this.keys = (Expression<ItemType>) exprs[1];
+			this.keysToCheck = (Expression<ItemType>) exprs[1];
 		this.place = parseResult.hasTag("place");
 		setNegated(matchedPattern == 1 || matchedPattern == 3);
 		return true;
@@ -56,37 +60,36 @@ public class CondHasBuildRestrictions extends Condition {
 	@SuppressWarnings("ConstantValue")
 	@Override
 	public boolean check(Event e) {
-		if (keys != null) {
-			Set<Namespaced> keysKeys = new HashSet<>();
-			for (ItemType c : keys.getArray(e)) {
-				for (ItemStack s : c.getAll()) {
-					keysKeys.add(s.getType().getKey());
-				}
+		if (keysToCheck == null)
+			return items.check(e, item -> place ? item.getItemMeta().hasPlaceableKeys() : item.getItemMeta().hasDestroyableKeys(), isNegated());
+		Set<Namespaced> keys = new HashSet<>();
+		for (ItemType item : keysToCheck.getArray(e)) {
+			for (ItemStack stack : item.getAll()) {
+				keys.add(stack.getType().getKey());
 			}
+		}
 
-			for (ItemType item : itemTypes.getArray(e)) {
-				ItemStack s = item.getRandom();
-				if (s.hasItemMeta()) {
-					ItemMeta meta = s.getItemMeta();
-					if (place ? meta.hasPlaceableKeys() : meta.hasDestroyableKeys()) {
-						Set<Namespaced> existingKeys = place ? meta.getPlaceableKeys() : meta.getDestroyableKeys();
-						for (Namespaced key : keysKeys) {
-							if (alreadyOn.contains(key))
-								return true;
-						}
+		for (ItemType item : items.getArray(e)) {
+			ItemStack stack = item.getRandom();
+			if (stack.hasItemMeta()) {
+				ItemMeta meta = stack.getItemMeta();
+				if (place ? meta.hasPlaceableKeys() : meta.hasDestroyableKeys()) {
+					Set<Namespaced> existingKeys = place ? meta.getPlaceableKeys() : meta.getDestroyableKeys();
+					for (Namespaced key : keys) {
+						if (!existingKeys.contains(key))
+							return false;
 					}
 				}
 			}
-			return false;
-		} else {
-			return itemTypes.check(e, item -> place ? item.getItemMeta().hasPlaceableKeys() : item.getItemMeta().hasDestroyableKeys(), isNegated());
 		}
+		return true;
     }
 
 	@SuppressWarnings("ConstantValue")
 	@Override
-	public String toString(@Nullable Event e, boolean b) {
-		return itemTypes.toString(e, b) + " has" + (isNegated() ? " no" : "") + (place ? " placeable" : " destroyable") + " keys" +
-			(keys == null ? "" : " of " + keys.toString(e, b));
+	public String toString(@Nullable Event event, boolean debug) {
+		if (keysToCheck == null)
+			return items.toString(event, debug) + " has" + (isNegated() ? " no" : "") + (place ? " placeable" : " destroyable") + " keys";
+		return "the" + (place ? " placeable" : " destroyable") + " keys of" + items.toString(event, debug) + (isNegated() ? " do not" : "") + " contain" + keysToCheck.toString(event, debug);
 	}
 }
