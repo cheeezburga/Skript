5025686ae46663d7cbbd2428d8f961a041b29562
cheeezburga <47320303+cheeezburga@users.noreply.github.com>
2024-04-27 01:35:25 +1000

    Adds armour stand extremities condition
    - Allows checking of an armour stand's arms or base plate
    - Not set on the pattern. Should there be two instead of one with the [:no]?
    - I feel like the check method could be more efficient

---
+++ src/main/java/ch/njol/skript/conditions/CondArmorStandExtremities.java
@@ -0,0 +1,76 @@
+/**
+ *   This file is part of Skript.
+ *
+ *  Skript is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Skript is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright Peter Güttinger, SkriptLang team and contributors
+ */
+package ch.njol.skript.conditions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Condition;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+@Name("Armor Stand - Has Extremities")
+@Description("Allows users to check the extremities of an armor stand (i.e. whether it has arms or a base plate).")
+@Examples({
+	"send true if {_armorstand} has arms",
+	"if {_armorstand} has no base plate"
+})
+@Since("INSERT VERSION")
+public class CondArmorStandExtremities extends Condition {
+
+	static {
+		Skript.registerCondition(CondArmorStandExtremities.class,
+			"%livingentities% (has|have) [:no] (:arms|[a] base[ ]plate)"
+		);
+	}
+
+	@SuppressWarnings("NotNullFieldNotInitialized")
+	private Expression<LivingEntity> entities;
+	private boolean arms;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+		entities = (Expression<LivingEntity>) exprs[0];
+		arms = parseResult.hasTag("arms");
+		setNegated(parseResult.hasTag("no"));
+		return true;
+	}
+
+	@Override
+	public boolean check(Event event) {
+		if (arms)
+			return entities.check(event,
+				stand -> stand instanceof ArmorStand && ((ArmorStand) stand).hasArms(), isNegated());
+		return entities.check(event,
+			stand -> stand instanceof ArmorStand && ((ArmorStand) stand).hasBasePlate(), isNegated());
+	}
+
+	@Override
+	public String toString(@Nullable Event event, boolean debug) {
+		return entities.toString(event, debug) + " has " + (isNegated() ? "no " : "") + (arms ? "arms" : "base plate");
+	}
+}
