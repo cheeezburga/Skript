e883d5b40956c7e04b8d209563eb13a3b9d7ca05
cheeezburga <47320303+cheeezburga@users.noreply.github.com>
2024-05-20 05:25:13 +1000

    Removed star imports

--- src/main/java/ch/njol/skript/effects/EffTooltip.java
+++ src/main/java/ch/njol/skript/effects/EffTooltip.java
@@ -20,7 +20,11 @@ package ch.njol.skript.effects;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.aliases.ItemType;
-import ch.njol.skript.doc.*;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.RequiredPlugins;
+import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
