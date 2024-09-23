116850464025271d52ff7af4fde69640ec7f3cb8
cheeezburga <47320303+cheeezburga@users.noreply.github.com>
2024-09-24 00:37:33 +1000

    Implements an enum to handle some logic in a nicer way and suggested changes

--- src/main/java/ch/njol/skript/expressions/ExprEntitySound.java
+++ src/main/java/ch/njol/skript/expressions/ExprEntitySound.java
@@ -11,6 +11,7 @@ import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.skript.util.Patterns;
 import ch.njol.util.Kleenean;
 import org.bukkit.Material;
 import org.bukkit.Sound;
@@ -32,48 +33,96 @@ import java.util.Objects;
 @RequiredPlugins("Spigot 1.19.2+")
 public class ExprEntitySound extends SimpleExpression<String> {
 
-	static {
-		if (Skript.methodExists(LivingEntity.class, "getDeathSound")) {
-			Skript.registerExpression(ExprEntitySound.class, String.class, ExpressionType.COMBINED,
-				"[the] (damage|hurt) sound[s] of %livingentities%",
-				"%livingentities%'[s] (damage|hurt) sound[s]",
+	public enum SoundType {
+		DAMAGE {
+			@Override
+			public @Nullable Sound getSound(LivingEntity entity, int height, ItemStack item, boolean bigOrSpeedy) {
+				return entity.getHurtSound();
+			}
+		},
+		DEATH {
+			@Override
+			public @Nullable Sound getSound(LivingEntity entity, int height, ItemStack item, boolean bigOrSpeedy) {
+				return entity.getDeathSound();
+			}
+		},
+		FALL {
+			@Override
+			public Sound getSound(LivingEntity entity, int height, ItemStack item, boolean bigOrSpeedy) {
+				if (height != -1)
+					return entity.getFallDamageSound(height);
+				else
+					return bigOrSpeedy ? entity.getFallDamageSoundBig() : entity.getFallDamageSoundSmall();
+			}
+		},
+		SWIM {
+			@Override
+			public Sound getSound(LivingEntity entity, int height, ItemStack item, boolean bigOrSpeedy) {
+				return entity.getSwimSound();
+			}
+		},
+		SPLASH {
+			@Override
+			public Sound getSound(LivingEntity entity, int height, ItemStack item, boolean bigOrSpeedy) {
+				return bigOrSpeedy ? entity.getSwimHighSpeedSplashSound() : entity.getSwimSplashSound();
+			}
+		},
+		EAT {
+			@Override
+			public Sound getSound(LivingEntity entity, int height, ItemStack item, boolean bigOrSpeedy) {
+				return entity.getEatingSound(item);
+			}
+		},
+		DRINK {
+			@Override
+			public Sound getSound(LivingEntity entity, int height, ItemStack item, boolean bigOrSpeedy) {
+				return entity.getDrinkingSound(item);
+			}
+		},
+		AMBIENT {
+			@Override
+			public @Nullable Sound getSound(LivingEntity entity, int height, ItemStack item, boolean bigOrSpeedy) {
+				return entity instanceof Mob mob ? mob.getAmbientSound() : null;
+			}
+		};
 
-				"[the] death sound[s] of %livingentities%",
-				"%livingentities%'[s] death sound[s]",
-
-				"[the] [high:(tall|high)|(low|normal)] fall damage sound[s] [from [[a] height [of]] %-number%] of %livingentities%",
-				"%livingentities%'[s] [high:(tall|high)|low:(low|normal)] fall [damage] sound[s] [from [[a] height [of]] %-number%]",
-
-				"[the] swim[ming] sound[s] of %livingentities%",
-				"%livingentities%'[s] swim[ming] sound[s]",
-
-				"[the] [fast:(fast|speedy)] splash sound[s] of %livingentities%",
-				"%livingentities%'[s] [fast:(fast|speedy)] splash sound[s]",
-
-				"[the] eat[ing] sound[s] of %livingentities% [(with|using|[while] eating [a]) %-itemtype%]",
-				"%livingentities%'[s] eat[ing] sound[s]",
-
-				"[the] drink[ing] sound[s] of %livingentities% [(with|using|[while] drinking [a]) %-itemtype%]",
-				"%livingentities%'[s] drink[ing] sound[s]",
-
-				"[the] ambient sound[s] of %livingentities%",
-				"%livingentities%'[s] ambient sound[s]");
-		}
+		public abstract @Nullable Sound getSound(LivingEntity entity, int height, ItemStack item, boolean bigOrSpeedy);
 	}
 
-	private static final int DAMAGE = 0;
-	private static final int DEATH = 2;
-	private static final int FALL = 4;
-	private static final int SWIM = 6;
-	private static final int SPLASH = 8;
-	private static final int EAT_WITH_ITEM = 10;
-	private static final int EAT = 11;
-	private static final int DRINK_WITH_ITEM = 12;
-	private static final int DRINK = 13;
-	private static final int AMBIENT = 14;
+	private static final Patterns<SoundType> patterns = new Patterns<>(new Object[][]{
+		{"[the] (damage|hurt) sound[s] of %livingentities%", SoundType.DAMAGE},
+		{"%livingentities%'[s] (damage|hurt) sound[s]", SoundType.DAMAGE},
 
-	private int soundPattern;
-	private boolean big;
+		{"[the] death sound[s] of %livingentities%", SoundType.DEATH},
+		{"%livingentities%'[s] death sound[s]", SoundType.DEATH},
+
+		{"[the] [high:(tall|high)|(low|normal)] fall damage sound[s] [from [[a] height [of]] %-number%] of %livingentities%", SoundType.FALL},
+		{"%livingentities%'[s] [high:(tall|high)|low:(low|normal)] fall [damage] sound[s] [from [[a] height [of]] %-number%]", SoundType.FALL},
+
+		{"[the] swim[ming] sound[s] of %livingentities%", SoundType.SWIM},
+		{"%livingentities%'[s] swim[ming] sound[s]", SoundType.SWIM},
+
+		{"[the] [fast:(fast|speedy)] splash sound[s] of %livingentities%", SoundType.SPLASH},
+		{"%livingentities%'[s] [fast:(fast|speedy)] splash sound[s]", SoundType.SPLASH},
+
+		{"[the] eat[ing] sound[s] of %livingentities% [(with|using|[while] eating [a]) %-itemtype%]", SoundType.EAT},
+		{"%livingentities%'[s] eat[ing] sound[s]", SoundType.EAT},
+
+		{"[the] drink[ing] sound[s] of %livingentities% [(with|using|[while] drinking [a]) %-itemtype%]", SoundType.DRINK},
+		{"%livingentities%'[s] drink[ing] sound[s]", SoundType.DRINK},
+
+		{"[the] ambient sound[s] of %livingentities%", SoundType.AMBIENT},
+		{"%livingentities%'[s] ambient sound[s]", SoundType.AMBIENT}
+	});
+
+	static {
+		if (Skript.methodExists(LivingEntity.class, "getDeathSound"))
+			Skript.registerExpression(ExprEntitySound.class, String.class, ExpressionType.COMBINED, patterns.getPatterns());
+	}
+
+	private boolean bigOrSpeedy;
+	@SuppressWarnings("NotNullFieldNotInitialized")
+	private SoundType soundType;
 	@SuppressWarnings("NotNullFieldNotInitialized")
 	private Expression<Number> height;
 	@SuppressWarnings("NotNullFieldNotInitialized")
@@ -84,49 +133,31 @@ public class ExprEntitySound extends SimpleExpression<String> {
 	@Override
 	@SuppressWarnings("unchecked")
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-		soundPattern = matchedPattern;
-		big = parseResult.hasTag("high") || parseResult.hasTag("fast");
-		if (matchedPattern == FALL || matchedPattern == FALL + 1)
+		soundType = patterns.getInfo(matchedPattern);
+		bigOrSpeedy = parseResult.hasTag("high") || parseResult.hasTag("fast");
+		if (soundType == SoundType.FALL)
 			height = (Expression<Number>) exprs[0];
-		if (matchedPattern == EAT_WITH_ITEM || matchedPattern == DRINK_WITH_ITEM)
+		if (soundType == SoundType.EAT || soundType == SoundType.DRINK)
 			item = (Expression<ItemType>) exprs[1];
-		entities = (Expression<LivingEntity>) ((matchedPattern == FALL) ? exprs[1] : exprs[0]);
+		entities = (Expression<LivingEntity>) ((soundType == SoundType.FALL) ? exprs[1] : exprs[0]);
 		return true;
 	}
 
 	@Override
-	@SuppressWarnings("ConstantValue")
 	protected String @Nullable [] get(Event event) {
+		//noinspection ConstantValue
 		int height = this.height == null ? -1 : this.height.getOptionalSingle(event).orElse(-1).intValue();
 
-		ItemStack defaultItem = new ItemStack(soundPattern == EAT_WITH_ITEM ? Material.COOKED_BEEF : Material.POTION);
+		ItemStack defaultItem = new ItemStack(soundType == SoundType.EAT ? Material.COOKED_BEEF : Material.POTION);
+		//noinspection ConstantValue
 		ItemStack item = this.item == null ? defaultItem : this.item.getOptionalSingle(event).map(ItemType::getRandom).orElse(defaultItem);
 
 		return entities.stream(event)
-			.map(entity -> getEntitySound(entity, height, item))
+			.map(entity -> soundType.getSound(entity, height, item, bigOrSpeedy))
 			.filter(Objects::nonNull)
 			.distinct()
 			.map(Sound::name)
 			.toArray(String[]::new);
-	}
-
-	private @Nullable Sound getEntitySound(LivingEntity entity, int height, ItemStack item) {
-		return switch (this.soundPattern) {
-			case DAMAGE, DAMAGE + 1 -> entity.getHurtSound();
-			case DEATH, DEATH + 1 -> entity.getDeathSound();
-			case FALL, FALL + 1 -> {
-				if (height != -1)
-					yield entity.getFallDamageSound(height);
-				else
-					yield big ? entity.getFallDamageSoundBig() : entity.getFallDamageSoundSmall();
-			}
-			case SWIM, SWIM + 1 -> entity.getSwimSound();
-			case SPLASH, SPLASH + 1 -> big ? entity.getSwimHighSpeedSplashSound() : entity.getSwimSplashSound();
-			case EAT, EAT_WITH_ITEM -> entity.getEatingSound(item);
-			case DRINK, DRINK_WITH_ITEM -> entity.getDrinkingSound(item);
-			case AMBIENT -> entity instanceof Mob mob ? mob.getAmbientSound() : null;
-			default -> null;
-		};
 	}
 
 	@Override
@@ -140,26 +171,30 @@ public class ExprEntitySound extends SimpleExpression<String> {
 	}
 
 	@Override
-	@SuppressWarnings("ConstantValue")
 	public String toString(@Nullable Event event, boolean debug) {
-		return switch (soundPattern) {
-			case DAMAGE, DAMAGE + 1 -> "damage";
-			case DEATH, DEATH + 1 -> "death";
-			case FALL, FALL + 1 -> {
+		String soundDescription = "unknown";
+		switch (soundType) {
+			case DAMAGE, DEATH, SWIM, AMBIENT -> soundDescription = soundType.name().toLowerCase();
+			case FALL -> {
+				//noinspection ConstantValue
 				if (this.height == null) {
-					yield big ? "high fall damage" : "normal fall damage";
+					soundDescription = bigOrSpeedy ? "high fall damage" : "normal fall damage";
 				} else {
-					yield "fall damage from a height of " + this.height.toString(event, debug);
+					soundDescription = "fall damage from a height of " + this.height.toString(event, debug);
 				}
 			}
-			case SWIM, SWIM + 1 -> "swim";
-			case SPLASH, SPLASH + 1 -> big ? "speedy splash" : "splash";
-			case EAT_WITH_ITEM, DRINK_WITH_ITEM -> (soundPattern == EAT_WITH_ITEM ? "eating" : "drinking") +
-				(this.item == null ? " with default item" : " " + this.item.toString(event, debug));
-			case EAT, DRINK -> soundPattern == EAT ? "eating" : "drinking";
-			case AMBIENT -> "ambient";
-			default -> "unknown";
-		} + " sound of " + entities.toString(event, debug);
+			case SPLASH -> soundDescription = bigOrSpeedy ? "speedy splash" : "splash";
+			case EAT, DRINK -> {
+				String action = soundType == SoundType.EAT ? "eating" : "drinking";
+				//noinspection ConstantValue
+				if (this.item == null) {
+					soundDescription = action;
+				} else {
+					soundDescription = action + " " + this.item.toString(event, debug);
+				}
+			}
+		}
+		return soundDescription + " sound of " + entities.toString(event, debug);
 	}
 
 }
