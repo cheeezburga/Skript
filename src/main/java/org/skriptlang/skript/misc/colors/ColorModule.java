ed49cfd0ec1f1f89c713fae51b02753f38760d3e
cheeezburga <47320303+cheeezburga@users.noreply.github.com>
2024-12-09 19:28:31 +1000

    Changes all 'colour's to 'color's and adds better descriptions and examples

---
+++ src/main/java/org/skriptlang/skript/misc/colors/ColorModule.java
@@ -0,0 +1,144 @@
+package org.skriptlang.skript.misc.colors;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.function.Functions;
+import ch.njol.skript.lang.function.Parameter;
+import ch.njol.skript.lang.function.SimpleJavaFunction;
+import ch.njol.skript.lang.util.SimpleLiteral;
+import ch.njol.skript.registrations.DefaultClasses;
+import ch.njol.skript.util.Color;
+import ch.njol.skript.util.ColorRGB;
+import ch.njol.util.coll.CollectionUtils;
+
+import java.io.IOException;
+
+public class ColorModule {
+
+	public static void load() throws IOException {
+		Skript.getAddonInstance().loadClasses("org.skriptlang.skript.misc", "colors");
+
+		Functions.registerFunction(new SimpleJavaFunction<Color>("shade", new Parameter[] {
+			new Parameter<>("color", DefaultClasses.COLOR, true, null),
+			new Parameter<>("amount", DefaultClasses.LONG, true, null),
+			new Parameter<>("hsl", DefaultClasses.BOOLEAN, true, new SimpleLiteral<>(false, true))
+		}, DefaultClasses.COLOR, true) {
+			@Override
+			public ColorRGB[] executeSimple(Object[][] params) {
+				Color color = (Color) params[0][0];
+				Long amount = (Long) params[1][0];
+				boolean hsl = (Boolean) params[2][0];
+				return CollectionUtils.array(hsl
+					? ColorUtils.shadeColorHSL(color, amount.intValue())
+					: ColorUtils.shadeColor(color, amount.intValue()));
+			}
+		}).description(
+			"Shades a given color by a given amount, with optional HSL-based shading.",
+			"The amount parameter ranges from 1 to 100, with lower values closer to the original color and higher values closer to black.",
+			"Inputs below 1 will default to shading by 1%."
+		).examples(
+			"set {_darkRed} to shade(red, 10)",
+			"set {_darkerRed} to shade(red, 20)",
+			"",
+			"function shadeExample(colour: colour, hsl: boolean = false):",
+				"\tloop 100 times:",
+					"\t\tset {_hex} to hex code of shade({_colour}, loop-value, {_hsl})",
+					"\t\tsend formatted \"%loop-value%: %{_hex}%████\" to all players",
+					"\t\twait 1 tick"
+		).since("INSERT VERSION");
+
+		Functions.registerFunction(new SimpleJavaFunction<Color>("tint", new Parameter[] {
+			new Parameter<>("color", DefaultClasses.COLOR, true, null),
+			new Parameter<>("amount", DefaultClasses.LONG, true, null),
+			new Parameter<>("hsl", DefaultClasses.BOOLEAN, true, new SimpleLiteral<>(false, true))
+		}, DefaultClasses.COLOR, true) {
+			@Override
+			public ColorRGB[] executeSimple(Object[][] params) {
+				Color color = (Color) params[0][0];
+				Long amount = (Long) params[1][0];
+				boolean hsl = (Boolean) params[2][0];
+				return CollectionUtils.array(hsl
+					? ColorUtils.tintColorHSL(color, amount.intValue())
+					: ColorUtils.tintColor(color, amount.intValue()));
+			}
+		}).description(
+			"Tints a given color by a given amount, with optional HSL-based shading.",
+			"The amount parameter ranges from 1 to 100, with lower values closer to the original color and higher values closer to white.",
+			"Inputs below 1 will default to tinting by 1%."
+		).examples(
+			"set {_lightRed} to tint(red, 10)",
+			"set {_lighterRed} to tint(red, 20)",
+			"",
+			"function tintExample(colour: colour, hsl: boolean = false):",
+				"\tloop 100 times:",
+					"\t\tset {_hex} to hex code of tint({_colour}, loop-value, {_hsl})",
+					"\t\tsend formatted \"%loop-value%: %{_hex}%████\" to all players",
+					"\t\twait 1 tick"
+		).since("INSERT VERSION");
+
+		Functions.registerFunction(new SimpleJavaFunction<Color>("colorBrightness", new Parameter[] {
+			new Parameter<>("color", DefaultClasses.COLOR, true, null),
+			new Parameter<>("amount", DefaultClasses.LONG, true, null)
+		}, DefaultClasses.COLOR, true) {
+			@Override
+			public ColorRGB[] executeSimple(Object[][] params) {
+				Color color = (Color) params[0][0];
+				Long amount = (Long) params[1][0];
+				return CollectionUtils.array(ColorUtils.adjustBrightness(color, amount.intValue()));
+			}
+		}).description(
+			"Adjusts the brightness of a color by a specified amount, ranging from -100 to 100.",
+			"Positive values increase brightness, with higher values approaching white, and negative values decrease brightness, with lower values approaching black.",
+			"Inputs beyond the range will be clamped to the nearest valid value."
+		).examples(
+			"set {_brighterRed} to colorBrightness(red, 10)",
+			"set {_darkerRed} to colorBrightness(red, -10)",
+			"",
+			"function brightnessExample(colour: colour):",
+				"\tloop integers from -100 to 100:",
+					"\t\tset {_hex} to hex code of colourBrightness({_colour}, loop-value)",
+					"\t\tsend formatted \"%loop-value%: %{_hex}%████\" to all players",
+					"\t\twait 1 tick"
+		).since("INSERT VERSION");
+
+		Functions.registerFunction(new SimpleJavaFunction<Color>("grayscale", new Parameter[] {
+			new Parameter<>("color", DefaultClasses.COLOR, true, null)
+		}, DefaultClasses.COLOR, true) {
+			@Override
+			public ColorRGB[] executeSimple(Object[][] params) {
+				Color color = (Color) params[0][0];
+				return CollectionUtils.array(ColorUtils.toGrayscale(color));
+			}
+		}).description(
+			"Converts a given color to its grayscale equivalent.",
+			"The resulting color retains its brightness but loses all hue, appearing as a shade of gray."
+		).examples(
+			"set {_redButGrayscale} to grayscale(red)",
+			"",
+			"function grayscaleExample():",
+				"\tloop all colours:",
+					"\t\tset {_hex} to hex code of grayscale(loop-value)",
+					"\t\tsend formatted \"%loop-value%: %{_hex}%████\" to all players"
+		).since("INSERT VERSION");
+
+		Functions.registerFunction(new SimpleJavaFunction<Color>("sepiatone", new Parameter[] {
+			new Parameter<>("color", DefaultClasses.COLOR, true, null)
+		}, DefaultClasses.COLOR, true) {
+			@Override
+			public ColorRGB[] executeSimple(Object[][] params) {
+				Color color = (Color) params[0][0];
+				return CollectionUtils.array(ColorUtils.toSepia(color));
+			}
+		}).description(
+			"Converts a given color to its sepiatone equivalent.",
+			"The resulting color mimics the warm, brownish look of vintage photographs."
+		).examples(
+			"set {_redButSepiatone} to sepiatone(red)",
+			"",
+			"function sepiatoneExample():",
+				"\tloop all colours:",
+					"\t\tset {_hex} to hex code of sepiatone(loop-value)",
+					"\t\tsend formatted \"%loop-value%: %{_hex}%████\" to all players"
+		).since("INSERT VERSION");
+	}
+
+}
