b6b4deee4b2d9a1da3d6ea3393ff9b607979336f
cheeezburga <47320303+cheeezburga@users.noreply.github.com>
2024-12-11 18:01:01 +1000

    Adds doc annotations to ExprHex

--- src/main/java/org/skriptlang/skript/misc/colors/ExprHex.java
+++ src/main/java/org/skriptlang/skript/misc/colors/ExprHex.java
@@ -1,9 +1,19 @@
 package org.skriptlang.skript.misc.colors;
 
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import ch.njol.skript.util.Color;
 import org.jetbrains.annotations.Nullable;
 
+@Name("Hex Code")
+@Description("Returns the hexadecimal code representing a given color(s).")
+@Examples(
+		"send formatted \"<%hex code of shade(red, 50)%>slightly darker red\" to all players"
+)
+@Since("INSERT VERSION")
 public class ExprHex extends SimplePropertyExpression<Color, String> {
 
 	static {
